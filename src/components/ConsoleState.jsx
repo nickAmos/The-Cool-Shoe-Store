b0a0950acf401ed66b7cec73d00c73e8ccc2e804
nickAmos <132029265+nickAmos@users.noreply.github.com>
2023-11-20 14:26:10 +1100

    Modals added

---
+++ src/components/ConsoleState.jsx
@@ -0,0 +1,9 @@
+
+function ConsoleState({state}) {
+
+    return(
+        <h1>{state ? 'True' : 'False'}</h1>
+    )
+}
+
+export default ConsoleState;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,41 @@
 import '../styling/Header.css'
 import 'semantic-ui-css/semantic.min.css'
 import { Icon } from 'semantic-ui-react';
+import Box from '@mui/material/Box';
+import 'semantic-ui-css/semantic.min.css'
+import Modal from '@mui/material/Modal';
+import { useState } from 'react';
+import ConsoleState from './ConsoleState';
+
+
+
+
+const styleFilter = {
+    position: 'absolute', top: '10%',
+    width: '22vw', height: '90vh', bgcolor: 'white', border: '2px solid #000', boxShadow: 24, p: 4,
+    borderRadius: 5, borderTopLeftRadius: 0, borderBottomLeftRadius: 0
+  };
+
+
+const styleCart = {
+    position: 'absolute', top: '10%', right: 0, borderRadius: 5,
+    width: '22vw', height: '90vh', bgcolor: 'white', border: '2px solid #000', p: 4, boxShadow: 24, 
+    borderTopRightRadius: 0, borderBottomRightRadius: 0, 
+  
+  };
+
+
+
 
 function Header() {
+
+const [openFilter, setOpenFilter] = useState(false);
+const handleOpenFilter = () => setOpenFilter(true);
+const handleCloseFilter = () => setOpenFilter(false);  
+
+const [openCart, setOpenCart] = useState(false);
+const handleOpenCart = () => setOpenCart(true);
+const handleCloseCart = () => setOpenCart(false);  
 
 
     return (
@@ -11,17 +44,30 @@ function Header() {
                 <div id='navbar'>
                     <div id='navbar-flex'>
                         <div id='left-box'>
-                            <div className='icon-container'>
-                                <Icon name='sliders horizontal' size='big'/>
-                            </div>
+                        <div className='icon-container' onClick={handleOpenFilter}>
+                            <Icon name='sliders horizontal' size='big'/>
+                        </div>
+                            <Modal open={openFilter} onClose={handleCloseFilter} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">  
+                                    <Box sx={styleFilter}>
+                                    </Box> 
+                            </Modal> 
+                         
+                            
+                            
+                            
                         </div>
                         <div id='middle-box'>
                             <h1>The Shoe Store.</h1>
                         </div>
                         <div id='right-box'>
-                            <div className='icon-container'>
+                            <div className='icon-container' onClick={handleOpenCart}>
                             <Icon name="cart" size='big'/>
                             </div>
+                            <Modal open={openCart} onClose={handleCloseCart} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">  
+                                    <Box sx={styleCart}>
+
+                                    </Box> 
+                            </Modal>
                         </div>
                     </div>
 
@@ -40,8 +86,26 @@ function Header() {
                 </div>
 
             </div>
+            <ConsoleState state={openCart} />
         </>
     )
 }
 
 export default Header;
+
+
+
+
+
+
+/* Old modal:
+(paste under "left-div")
+                <div className='icon-container' onClick={handleOpenFilter}>
+                    <Icon name='sliders horizontal' size='big'/>
+                        </div>
+                            <Modal open={openFilter} onClose={handleCloseFilter} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">  
+                                    <Box sx={styleFilter}>
+                                    </Box> 
+                            </Modal> 
+                </div>   
+*/   
