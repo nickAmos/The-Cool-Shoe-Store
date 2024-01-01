62689e3e77cc1dafe15cb76dfd8f2579ad46c802
nickAmos <132029265+nickAmos@users.noreply.github.com>
2024-01-01 10:41:07 +1000

    Starting Checkout component

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import '../styling/Checkout.css';
 
 export default function Checkout({cart, filterCart}) {
 
@@ -6,20 +7,38 @@ export default function Checkout({cart, filterCart}) {
 
     return(
         <>
-            <h1>checkout</h1>
-            <div>
-            {cart.map((shoe, index) => {
-    return (
-        <div key={index}>
-        <p>{shoe.name} size: {shoe.size} id:{shoe.id}</p>
-        <button onClick={() => filterCart(shoe.id)}>clear</button>
-        </div> )})}
+        <div id="checkout-container-flex">
+            <div id="checkout-flex-child">
+                <div id="checkout-img-section"></div>
+                <div id="checkout-shoe-section">
+                <div>
+                {cart.map((shoe, index) => {
+                    return ( <div key={index}>
+                                <p>{shoe.name} size: {shoe.size} id:{shoe.id}</p>
+                                <button onClick={() => filterCart(shoe.id)}>clear</button>
+                            </div> )})}
             
-                 </div>
+            </div>
+                </div>
+
+                    
+
+            
+
+            </div>
+
+            </div>
+
             <Link to='/'>
                 home
             </Link>
         </>
     )
 }
+
+
+
+/*map of shoes should be shown on the right, they should be clickable
+ to bring up an image of the shoe that appears on the right.
+ sub totals and payment options can be seen below */
 
